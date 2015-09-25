c9398d7d188121aec2209eeb0e945c6d1e9e0b4e
landyliuqiqi <603556820@qq.com>
2015-09-25 10:40:12 +0800

    Update main.js

--- practice_v01/src/main.js
+++ practice_v01/src/main.js
@@ -1,1 +1,10 @@
+
 //  Your code here
+var Person = function (age, name) {
+    this._age = age;
+    this._name = name;
+};
+
+Person.prototype.introduce = function () {
+    return "My name is " + this._name + ". I am " + this._age + " years old.";
+};
